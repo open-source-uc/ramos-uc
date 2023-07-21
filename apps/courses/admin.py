d94917bb08942d37ca2194212d0022476e53ba42
endlessloop2 <mateo@endlessloop.me>
2023-07-21 22:06:32 +0000

    Rollback apps/courses/admin.py

--- apps/courses/admin.py
+++ apps/courses/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, Section, FullSchedule
+from .models import Course, Section
 
 
 class SectionAdmin(admin.ModelAdmin):
@@ -11,4 +11,3 @@ class SectionAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(Section, SectionAdmin)
-admin.site.register(FullSchedule)
