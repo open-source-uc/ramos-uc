0944d17bdf57e815619702404ab6eae7fac79e19
Benjamín Vicente <62021328+benjavicente@users.noreply.github.com>
2023-07-30 22:01:51 +0000

    chore(handle): show line info if traceback

--- apps/bc_scraper/actions/errors.py
+++ apps/bc_scraper/actions/errors.py
@@ -1,6 +1,4 @@
-from datetime import datetime
 import json
-from django.conf import settings
 import logging
 
 log = logging.getLogger("scraper")
@@ -8,4 +6,8 @@ log = logging.getLogger("scraper")
 
 def handle(context, err):
     log.error("%s", err)
+    if isinstance(err, Exception) and err.__traceback__:
+        filename = err.__traceback__.tb_frame.f_code.co_filename
+        line_number = err.__traceback__.tb_lineno
+        log.error("Error at %s:%s", filename, line_number)
     log.error("Context: %s", json.dumps(context, indent=2))
